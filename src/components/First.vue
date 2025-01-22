a660bfd928febc587094a4057825d3fe42a880a2
Arvin Cris <arvincris97@gmail.com>
2025-01-22 20:59:22 +0800

    first page

--- src/components/First.vue
+++ src/components/First.vue
@@ -1,8 +1,47 @@
-<script setup>
+<script>
+    export default {
+        data() {
+            return {
+                formData: {
+                    name: "",
+                    message: ""
+                }
+            }
+        },
+        methods: {
+            async submitForm() {
+                const response = await fetch('http://119.9.108.155/apitest/index.php?c=create', {
+                    method: 'POST',
+                    headers: {
+                    'Content-Type': 'application/json',
+                    'apikey': '2025frontdeveloper'
+                    },
+                    body: JSON.stringify(this.formData)
+                });
+                
+                const result = await response.json();
+
+                console.log(result);
+            }
+        }
+    }
 </script>
 
 <template>
-  <h1>First Page</h1>
+  <div>
+    <h1>Submit a Message</h1>
+    <form @submit.prevent="submitForm">
+        <div>
+            <label for="name">Name: </label>
+            <input type="text" v-model="formData.name" required/>
+        </div>
+        <div>
+            <label for="message">Message: </label>
+            <textarea type="text" v-model="formData.message" required/>
+        </div>
+        <button type="submit">Submit</button>
+    </form>
+  </div>
 
   <div class="card">
     <router-link to="/">
