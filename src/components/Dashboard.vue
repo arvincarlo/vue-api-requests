afbe052af5c5a73e4a18f65c2dbf9c3a1e7a2298
Arvin Cris <arvincris97@gmail.com>
2025-01-22 17:38:18 +0800

    building of UI

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+  <h1>Stamford Inc. examination</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">First Page</button>
+  </div>
+  <div class="card">
+    <button type="button" @click="count++">Second Page</button>
+  </div>
+
+  <p>
+    Examinee: Arvin Carlo Cris
+  </p>
+</template>
+
+<style scoped>
+</style>
